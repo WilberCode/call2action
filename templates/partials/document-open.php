a04e2540838d7791048b14951efe11c7ab5d38fb
wilber <parionahuaraca@gmail.com>
2021-01-26 00:36:31 -0500

    cambios

--- templates/partials/document-open.php
+++ templates/partials/document-open.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= wp_get_document_title() ?></title>
+   <!--  <title><? //= wp_get_document_title() ?></title> -->
     <script>document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/, 'js');</script>
     <meta name="keywords" content="call2action, Grupo Tendenze Comunicaciones, Asesoría, Packs Online, Diseños, 
     Tendencias,Cambios, marketing, publicidad,Creativas y digitales,Pack Nuevos Negocios,
