f29ace060b2deb0bde7cac8a0b45beccc185e2d3
wilber <parionahuaraca@gmail.com>
2021-01-26 01:30:35 -0500

    cambios

--- templates/partials/document-open.php
+++ templates/partials/document-open.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+          content="width=device-width,initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
    <!--  <title><? //= wp_get_document_title() ?></title> -->
     <script>document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/, 'js');</script>
@@ -11,6 +11,15 @@
     Tendencias,Cambios, marketing, publicidad,Creativas y digitales,Pack Nuevos Negocios,
     Pack Negocio Digital, Pack Registros & Leads, Pack Redes Sociales, Video Redes Sociales, Diseños off & online"> 
     <?php wp_head() ?>    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-185373616-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-185373616-1');
+    </script>
 
 </head>
 <body <?php body_class() ?> >
